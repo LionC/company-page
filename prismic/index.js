b700a536f5af56829905ba10e2d5698845c23b92
Leon Strauss <leon@ilu.st>
2016-11-10 16:36:15 +0100

    lots of work, templating wrks now, some sectinos are already served by the cms

---
+++ prismic/index.js
@@ -0,0 +1,28 @@
+const cache = require('persistent-cache');
+const Prismic = require('prismic.io');
+const util = require('util')
+
+var exp = {
+    products: [],
+    team: [],
+    blog: []
+};
+
+const typeMap = {
+    'team-member': 'team',
+    'product': 'products',
+    'blog-post': 'blog'
+};
+
+exp.ready = Prismic.api("https://aco-company-page.prismic.io/api").then(function(api) {
+    //return api.query(""); // An empty query will return all the documents
+    return api.query("");
+}).then(function(response) {
+    response.results.forEach(function (doc) {
+        exp[typeMap[doc.type]].push(doc);
+    });
+}).catch(function(err) {
+    console.log("Something went wrong: ", err);
+});
+
+module.exports = exp;
--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 var assert = require('assert');
 var express = require('express');
 
+var prismicData = require('./prismic');
+
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname + '/static'));
-
-app.get('/', render);
+app.get('/', function(req, res) { res.redirect('/en/aboutus') });
+app.get('/:lang/:page', render);
 
 function render(req, res) {
-    res.render('index');
+    res.render('index', {
+        page: req.params.page,
+        products: prismicData.products,
+        team: prismicData.team,
+        blog: prismicData.blog,
+        lang: req.params.lang
+    });
 }
 
-var server = app.listen(PORT, function() {
-    var host = server.address().address;
-    var port = server.address().port;
+function startServer() {
+    var server = app.listen(PORT, function() {
+        var host = server.address().address;
+        var port = server.address().port;
 
-    console.log('Listening on' + host + ':' + port);
-});
+        console.log('Listening on' + host + ':' + port);
+    });
+}
+
+prismicData.ready.then(startServer);
